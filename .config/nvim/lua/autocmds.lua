7a3d7555b118844fd3d8aa30630cb781aa4866d6
Dung Ngo <quocdungngole@gmail.com>
2026-02-02 13:10:42 +0700

    feat: Make command, permanently make alt the layer mod

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -4,3 +4,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
     pattern = '*',
 })
+
+-- Crude Make command
+vim.api.nvim_create_user_command('Make', function(opts)
+    local command = table.concat(opts.fargs, ' ') or vim.o.makeprg
+    vim.cmd([[below terminal ]] .. command)
+    vim.api.nvim_win_set_height(0, math.floor(vim.api.nvim_win_get_height(0) / 2))
+end, {
+        nargs = '*',
+        complete = function(ArgLead, CmdLine, CursorPos)
+            -- return completion candidates as a list-like table
+            return { "foo", "bar", "baz" }
+        end,
+    })
