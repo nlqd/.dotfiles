6d5ae2451b3c26b14c425609d2bb3ba177a04514
Dung Ngo <dung.ngo@eastagile.com>
2026-03-30 11:58:24 +0700

    feat: add nvim 0.12 one file config

--- .config/nvim-0.12/init.lua
+++ .config/nvim-0.12/init.lua
@@ -21,7 +21,7 @@ vim.o.completeopt = 'fuzzy,menuone,noselect'
 vim.o.termguicolors = true
 vim.o.splitright = true
 vim.o.list = true
-vim.o.listchars = "tab:» ,trail:·,nbsp:␣"
+vim.o.listchars = 'tab:» ,trail:·,nbsp:␣'
 vim.o.inccommand = 'split'
 vim.o.cursorline = true
 vim.o.scrolloff = 10
@@ -29,9 +29,9 @@ vim.o.exrc = true
 vim.o.hlsearch = true
 
 -- Keymaps
-vim.keymap.set('v', '<leader>y', '\"+y', { desc = '[Y]ank to system' })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', '<leader>y', '\'+y', { desc = '[Y]ank to system' })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set('n', '<C-c>', '<cmd>nohlsearch<CR>')
@@ -48,22 +48,22 @@ vim.api.nvim_create_user_command('Make', function(opts)
     vim.api.nvim_win_set_height(0, math.floor(vim.api.nvim_win_get_height(0) / 2))
 end, { nargs = '*' })
 
--- helper
+-- packages
 local gh = function(x) return 'https://github.com/' .. x end
-vim.pack.add({ gh('folke/which-key.nvim') })
-vim.pack.add({ gh('iamcco/markdown-preview.nvim') })
-vim.pack.add({ gh('nvim-lua/plenary.nvim') })
-vim.pack.add({ gh('nvim-tree/nvim-web-devicons') })
-vim.pack.add({ gh('stefandtw/quickfix-reflector.vim') })
-vim.pack.add({ gh('tpope/vim-fugitive') })
-vim.pack.add({ gh('tpope/vim-projectionist') })
-vim.pack.add({ gh('tpope/vim-sleuth') })
+vim.pack.add({ gh'folke/which-key.nvim' })
+vim.pack.add({ gh 'iamcco/markdown-preview.nvim' })
+vim.pack.add({ gh'nvim-lua/plenary.nvim' })
+vim.pack.add({ gh'nvim-tree/nvim-web-devicons' })
+vim.pack.add({ gh'stefandtw/quickfix-reflector.vim' })
+vim.pack.add({ gh'tpope/vim-fugitive' })
+vim.pack.add({ gh'tpope/vim-projectionist' })
+vim.pack.add({ gh'tpope/vim-sleuth' })
 
 -- lsp
 vim.pack.add({
-    gh('neovim/nvim-lspconfig'),
-    gh('j-hui/fidget.nvim'),
-    gh('nvimtools/none-ls.nvim'),
+    gh'neovim/nvim-lspconfig',
+    gh'j-hui/fidget.nvim',
+    gh'nvimtools/none-ls.nvim',
 })
 require('fidget').setup({})
 require('null-ls').setup({
@@ -74,49 +74,39 @@ vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, { desc = '[G]o and [F]orma
 vim.lsp.config('lua_ls', { settings = { Lua = {
     runtime = { version = 'LuaJIT' },
     diagnostics = { globals = { 'vim', 'require' }},
-    workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+    workspace = { library = vim.api.nvim_get_runtime_file('', true) },
 }}})
 vim.lsp.enable({ 'lua_ls', 'ts_ls', 'pyright', 'rust_analyzer', 'gopls' })
 
 -- mason
 vim.pack.add({
-    gh('williamboman/mason.nvim'),
-    gh('williamboman/mason-lspconfig.nvim'),
+    gh'williamboman/mason.nvim',
+    gh'williamboman/mason-lspconfig.nvim',
 })
 require('mason').setup()
 require('mason-lspconfig').setup()
 
 -- oil
-vim.pack.add({ gh('stevearc/oil.nvim') })
+vim.pack.add({ gh'stevearc/oil.nvim' })
 require('oil').setup({
     default_file_explorer = true,
-    columns = { "icon" },
-    constrain_cursor = "editable",
+    columns = {},
     delete_to_trash = true,
     view_options = {
         show_hidden = true,
         natural_order = true,
         is_always_hidden = function(name, _)
-            return name == ".."
+            return name == '..'
         end
     },
     win_options = {
         wrap = true,
     },
-    keymaps = {
-        ["gd"] = function ()
-            if #require("oil.config").columns == 1 then
-                require("oil").set_columns({ "icon", "premissions", "size", "mtime" })
-            else
-                require("oil").set_columns({ "icon" })
-            end
-        end
-    },
 })
 vim.keymap.set('n', '<leader>-', '<CMD>Oil<CR>')
 
 -- gitsigns
-vim.pack.add({ gh('lewis6991/gitsigns.nvim') })
+vim.pack.add({ gh'lewis6991/gitsigns.nvim' })
 require('gitsigns').setup({
     current_line_blame = true,
     on_attach = function()
@@ -145,30 +135,30 @@ require('gitsigns').setup({
 })
 
 -- indent-blankline
-vim.pack.add({gh('lukas-reineke/indent-blankline.nvim'),})
+vim.pack.add({gh'lukas-reineke/indent-blankline.nvim',})
 require('ibl').setup({
     debounce = 100,
-    indent = { char = "┊" },
-    whitespace = { highlight = { "Whitespace", "NonText" } },
-    scope = { exclude = { language = { "lua" } } },
+    indent = { char = '┊' },
+    whitespace = { highlight = { 'Whitespace', 'NonText' } },
+    scope = { exclude = { language = { 'lua' } } },
 })
 
 -- multi.lua
-vim.pack.add({ gh('jake-stewart/multicursor.nvim') })
-local mc = require("multicursor-nvim")
+vim.pack.add({ gh'jake-stewart/multicursor.nvim' })
+local mc = require('multicursor-nvim')
 mc.setup()
 -- Add or skip cursor above/below the main cursor.
-vim.keymap.set({ "n", "x" }, "<up>", function() mc.lineAddCursor(-1) end)
-vim.keymap.set({ "n", "x" }, "<down>", function() mc.lineAddCursor(1) end)
-vim.keymap.set({ "n", "x" }, "<leader><up>", function() mc.lineSkipCursor(-1) end)
-vim.keymap.set({ "n", "x" }, "<leader><down>", function() mc.lineSkipCursor(1) end)
+vim.keymap.set({ 'n', 'x' }, '<up>', function() mc.lineAddCursor(-1) end)
+vim.keymap.set({ 'n', 'x' }, '<down>', function() mc.lineAddCursor(1) end)
+vim.keymap.set({ 'n', 'x' }, '<leader><up>', function() mc.lineSkipCursor(-1) end)
+vim.keymap.set({ 'n', 'x' }, '<leader><down>', function() mc.lineSkipCursor(1) end)
 -- Add or skip adding a new cursor by matching word/selection
-vim.keymap.set({ "n", "x" }, "<leader>n", function() mc.matchAddCursor(1) end)
-vim.keymap.set({ "n", "x" }, "<leader>s", function() mc.matchSkipCursor(1) end)
-vim.keymap.set({ "n", "x" }, "<leader>N", function() mc.matchAddCursor(-1) end)
-vim.keymap.set({ "n", "x" }, "<leader>S", function() mc.matchSkipCursor(-1) end)
+vim.keymap.set({ 'n', 'x' }, '<leader>n', function() mc.matchAddCursor(1) end)
+vim.keymap.set({ 'n', 'x' }, '<leader>s', function() mc.matchSkipCursor(1) end)
+vim.keymap.set({ 'n', 'x' }, '<leader>N', function() mc.matchAddCursor(-1) end)
+vim.keymap.set({ 'n', 'x' }, '<leader>S', function() mc.matchSkipCursor(-1) end)
 mc.addKeymapLayer(function(layerSet)
-    layerSet("n", "<esc>", function()
+    layerSet('n', '<esc>', function()
         if not mc.cursorsEnabled() then
             mc.enableCursors()
         else
@@ -179,9 +169,9 @@ end)
 
 -- treesitter
 vim.pack.add({
-    gh('nvim-treesitter/nvim-treesitter'),
-    gh('nvim-treesitter/nvim-treesitter-context'),
-    gh('nvim-treesitter/nvim-treesitter-textobjects'),
+    gh'nvim-treesitter/nvim-treesitter',
+    gh'nvim-treesitter/nvim-treesitter-context',
+    gh'nvim-treesitter/nvim-treesitter-textobjects',
 })
 require('treesitter-context').setup({
     multiline_threshold = 1,
@@ -221,17 +211,17 @@ require('treesitter-context').setup({
 -- })
 
 -- todo-comments
-vim.pack.add({ gh('folke/todo-comments.nvim') })
+vim.pack.add({ gh'folke/todo-comments.nvim' })
 require('todo-comments').setup({ signs = false })
 
 -- undotree
-vim.pack.add({ gh('mbbill/undotree') })
+vim.pack.add({ gh'mbbill/undotree' })
 vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<CR>', { desc = '[U]ndoTree' })
 
 -- vim-slime.lua
-vim.pack.add({ gh('jpalardy/vim-slime') })
+vim.pack.add({ gh'jpalardy/vim-slime' })
 vim.g.slime_no_mappings = 1
-vim.g.slime_cell_delimiter = "# %%"
+vim.g.slime_cell_delimiter = '# %%'
 vim.g.slime_target = 'tmux'
 vim.g.slime_bracketed_paste = 1
 vim.keymap.set('n', '<leader>s',  '<Plug>SlimeRegionSend')
@@ -239,15 +229,22 @@ vim.keymap.set('n', '<leader>ss', '<Plug>SlimeLineSend')
 vim.keymap.set('n', '<leader>sc', '<Plug>SlimeSendCell')
 
 -- vimtex
-vim.pack.add({ gh('lervag/vimtex') })
-vim.g.vimtex_view_method = "zathura"
+vim.pack.add({ gh'lervag/vimtex' })
+vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_quickfix_open_on_warning = 0
 
 -- dadbod
 vim.pack.add({
-    gh('tpope/vim-dadbod'),
-    gh('kristijanhusak/vim-dadbod-completion'),
-    gh('kristijanhusak/vim-dadbod-ui'),
+    gh'tpope/vim-dadbod',
+    gh'kristijanhusak/vim-dadbod-completion',
+    gh'kristijanhusak/vim-dadbod-ui',
 })
 vim.g.db_ui_winwidth = 30
 vim.g.db_ui_use_nerd_fonts = 1
+
+-- mini
+vim.pack.add({ gh'nvim-mini/mini.nvim' })
+require('mini.pick').setup({})
+vim.keymap.set('n', '<leader>sf', '<cmd>Pick files<cr>')
+vim.keymap.set('n', '<leader>sg', '<cmd>Pick grep_live<cr>')
+vim.keymap.set('n', '<leader>sr', '<cmd>Pick resume<cr>')
